d29ec35a5fb4381256751cdebbfb8c49ac0d6fb3
Edogawa-plus <jiuxiaog@outlook.com>
2023-11-10 23:39:48 +0800

    #Update layout

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,140 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView, useRouter, useRoute } from 'vue-router'
+import { Menu as IconMenu, Expand, Fold, Document, Setting, House } from '@element-plus/icons-vue'
+import { ref, onMounted, watch } from 'vue'
+
+const router = useRouter()
+const route = useRoute()
+
+const isCollpase = ref<boolean>(false)
+const indexActive = ref<string>('')
+
+onMounted(() => {
+  indexActive.value = 'home'
+})
+
+watch(
+  () => route.name,
+  (val) => {
+    indexActive.value = val
+  }
+)
+
+function menuSelect(index: string) {
+  router.push({ name: index })
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <el-container class="app-content">
+    <el-header>
+      <h1>Vue3 Vite TS Demo</h1>
+    </el-header>
+    <el-container class="body-container">
+      <el-aside :class="{ 'is-collapse': isCollpase }" width="200px">
+        <div class="collpase-container" @click="isCollpase = !isCollpase">
+          <el-icon v-if="isCollpase" size="18">
+            <Expand />
+          </el-icon>
+          <el-icon v-else size="18">
+            <Fold />
+          </el-icon>
+        </div>
+        <el-menu
+          active-text-color="#ffd04b"
+          background-color="#27272a"
+          class="el-menu-vertical-demo"
+          :default-active="indexActive"
+          text-color="#fff"
+          :collapse="isCollpase"
+          unique-opened
+          @select="menuSelect"
+        >
+          <el-menu-item index="home">
+            <el-icon>
+              <House />
+            </el-icon>
+            主页
+          </el-menu-item>
+          <el-sub-menu index="feature">
+            <template #title>
+              <el-icon>
+                <IconMenu />
+              </el-icon>
+              <span>功能组件</span>
+            </template>
+            <el-menu-item index="pdf-demo"> PDF导出 </el-menu-item>
+          </el-sub-menu>
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <span>Navigator Two</span>
+          </el-menu-item>
+          <el-menu-item index="3">
+            <el-icon>
+              <document />
+            </el-icon>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon>
+              <setting />
+            </el-icon>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <RouterView />
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+<style lang="scss" scoped>
+.app-content {
+  height: 100%;
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+  .el-header {
+    position: absolute;
+    display: grid;
+    place-content: center start;
+    width: 100%;
+    color: var(--white-text);
+    background-color: var(--bg-dark-theme);
+    border-bottom: 1px solid var(--grey-border-box);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+  .body-container {
+    margin-top: 60px;
+    height: calc(100% - 60px);
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+    .el-aside {
+      color: var(--white-text);
+      background-color: var(--bg-dark-theme);
 
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
+      &.is-collapse {
+        width: auto;
 
-    padding: 1rem 0;
-    margin-top: 1rem;
+        .collpase-container {
+          place-content: center;
+        }
+      }
+
+      .collpase-container {
+        display: flex;
+        place-content: center end;
+        padding-right: 2px;
+        cursor: pointer;
+      }
+
+      .el-menu--vertical {
+        border-right: 1px solid var(--bg-dark-theme);
+
+        > :deep(li) {
+          width: var(--el-aside-width);
+        }
+      }
+    }
   }
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(),
+  // history: createWebHashHistory(),
   routes: [
     {
       path: '/',
@@ -10,12 +12,17 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/feature',
+      name: 'feature',
+      component: () => import('@/views/HomeIndex.vue'),
+      redirect: { name: 'pdf-demo' },
+      children: [
+        {
+          path: 'pdf-demo',
+          name: 'pdf-demo',
+          component: () => import('@/views/feature/PDFDemo.vue'),
+        }
+      ]
     }
   ]
 })
